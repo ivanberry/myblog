ffbe4e339122fd689b7b7cca8874239da9381342
Tab_F2E <tab@Tab-F2Es-MacBook-Pro.local>
2017-06-18 06:40:59 +0800

    dockerize python and db env

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,34 @@
 # project/__init__.py
+import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
+# set config with docker env settings
+app_setting = os.getenv('APP_SETTINGS')
+app.config.from_object(app_setting)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+        __tableusers_ = "users"
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        username = db.Column(db.String(128), nullable=False)
+        email = db.Column(db.String(128), nullable=False)
+        active = db.Column(db.Boolean(), default=False, nullable=False)
+        created_at = db.Column(db.DateTime, nullable=False)
+
+        def __int__(self, username, email):
+            self.username = username
+            self.email = email
+            self.created_at = datetime.datetime.now()
+
+# routes
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
--- project/config.py
+++ project/config.py
@@ -1,18 +1,24 @@
 # project/config.py
 
+import os
+
 class BaseConfig:
     '''Base configurations'''
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
     '''Development configuration'''
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     '''Testing configuration'''
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class ProductionConfig(BaseConfig):
     '''prodction configration'''
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
