4bfeefe76985e5472addd40dd2b23ddc9df2b4e5
Tab_F2E <tab@Tab-F2Es-MacBook-Pro.local>
2017-06-18 15:44:48 +0800

    refactor app with flask blueprint, But I did\'t understand it well

--- manage.py
+++ manage.py
@@ -1,10 +1,21 @@
 # manage.py
 
 from flask_script import Manager
-from project import app, db
+from project import create_app, db
 import unittest
+from project.api.models import User
 
+app = create_app()
 manager = Manager(app)
+
+@manager.command
+def test():
+    """ Runs the tests without code coverage. """
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 @manager.command
@@ -13,17 +24,6 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
-
-
-@manager.command
-def test():
-    """ Runs the tests without code coverage. """
-
-    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
 
 
 if __name__ == '__main__':
