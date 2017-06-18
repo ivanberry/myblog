90e4bb918e4572eb73704a334118a8271aaa5366
Tab_F2E <tab@Tab-F2Es-MacBook-Pro.local>
2017-06-18 16:25:10 +0800

    update error handlers

--- project/api/views.py
+++ project/api/views.py
@@ -1,6 +1,9 @@
 #project/api/views.py
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, make_response
+from project.api.models import User
+from project import db
+from sqlalchemy import exc
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -10,3 +13,50 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+@users_blueprint.route('/users', methods=['POST'])
+def add_user():
+    post_data = request.get_json()
+    # handler the empty post
+    if not post_data:
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+
+        # 返回
+        return make_response(jsonify(response_object)), 400
+    username = post_data.get('username')
+    email = post_data.get('email')
+    try:
+        # handler the duplicate email post
+        user = User.query.filter_by(email=email).first()
+
+        # not duplicate email
+        if not user:
+            db.session.add(User(username=username, email=email))
+            db.session.commit()
+
+            response_object = {
+                'status': 'success',
+                'message': f'{email} was added!'
+            }
+            return make_response(jsonify(response_object)), 201
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Sorry, That email has already exist.'
+            }
+            return make_response(jsonify(response_object)), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return make_response(jsonify(response_object)), 400
+
+
+
+
+
--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -13,3 +13,106 @@ class TestUsersService(BaseTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
+
+    def test_add_user(self):
+        '''Ensure a new user can be added to the database'''
+
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username = 'tab',
+                    email='tab@gmail.com'
+                )),
+                content_type='application/json'
+            )
+
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('tab@gmail.com was added!', data['message'])
+            self.assertIn('success', data['status'])
+
+    def test_add_user_invalid_json(self):
+        '''Ensure error is thrown if the JSON object is empty'''
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict()),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_invalid_json_keys(self):
+        '''Ensure error is thrown if the JSON object does not have username key'''
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(emai='tab@gmail.com')),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_duplicate_user(self):
+        '''Ensure error is thrown if the email already exits.'''
+        with self.client:
+            self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='tab',
+                    email='tab@gmail.com'
+                )),
+                content_type='application/json'
+            )
+
+            # 模拟post请求，绑定'users'接口返回的数据
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='shrint',
+                    email='tab@gmail.com'
+                )),
+                content_type='application/json'
+            )
+
+            # 解码返回的数据
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Sorry, That email has already exist.', data['message'])
+            self.assertIn('fail', data['status'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
